5f6dd7710f03edc690b6f94ae01d6a631196fe5f
Michael Albertz <michael@prio.one>
2024-02-08 13:41:53 +0100

    import astro plugins of commonly used languages

--- astro_user/plugins/community.lua
+++ astro_user/plugins/community.lua
@@ -6,5 +6,21 @@ return {
 
   { import = "astrocommunity.colorscheme.rose-pine" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
-  { import = "astrocommunity.pack.python-ruff" }
+  { import = "astrocommunity.motion.nvim-surround" },
+
+  -- language packs: https://github.com/AstroNvim/astrocommunity/tree/main/lua/astrocommunity/pack
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.python-ruff" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.docker" },
+
+  { import = "astrocommunity.pack.typescript-all-in-one" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.markdown" },
+
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.toml" },
 }
