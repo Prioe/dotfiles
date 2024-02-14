44c7eadb7b2d8f7fcabacf7266f739f73c468af0
Michael Albertz <michael@prio.one>
2024-02-14 08:50:22 +0100

    remove some unused zsh config

--- zsh/scripts.zsh
+++ zsh/scripts.zsh
@@ -1,24 +1,25 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
+# vim:ft=sh
 
 # this pushes my local obsidian vault to a remote git repo
-push_notes () {
-  # format: 12.02.2024, 16:06
-  T="$(date +'%d.%m.%Y, %H:%M')"
-  H="$(hostname)"
-  cd ~/notes
-  git add .
-  git commit -m "Pushing notes from $H at $T"
-  git push
-  cd -
+push_notes() {
+	# format: 12.02.2024, 16:06
+	T="$(date +'%d.%m.%Y, %H:%M')"
+	H="$(hostname)"
+	cd ~/notes || exit
+	git add .
+	git commit -m "Pushing notes from $H at $T"
+	git push
+	cd - || exit
 }
 
-pull_notes () {
-  cd ~/notes
-  git pull
-  cd -
+pull_notes() {
+	cd ~/notes || exit
+	git pull
+	cd - || exit
 }
 
-sync_notes () {
-  pull_notes
-  push_notes
+sync_notes() {
+	pull_notes
+	push_notes
 }
