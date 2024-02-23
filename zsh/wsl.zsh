422342ced1611ec2348138bf7da9a293eaf36ed0
Michael Albertz <michael@prio.one>
2024-02-23 09:46:33 +0100

    add win32yank to wsl config to support neovim system clipboard

--- zsh/wsl.zsh
+++ zsh/wsl.zsh
@@ -5,6 +5,20 @@ if [ ! -f "/proc/sys/fs/binfmt_misc/WSLInterop" ]; then
 	exit 1
 fi
 
+add_winget_package_path() {
+	local -r package_name="$1"
+	local -r package_path="$(fd "$package_name" -p -t d "$windows_root/Users/$windows_user/AppData/Local/Microsoft/WinGet")"
+
+	if [ -d "$package_path" ]; then
+		export PATH="$PATH:$package_path"
+		return 0
+	else
+		echo "Could not find $package_name in WindowsApps"
+	fi
+
+	return 1
+}
+
 # We set appendWindowsPath=false in /etc/wsl.conf to avoid Windows paths in $PATH
 # Some programs are very helpful and we add them to our path here
 # add Microsoft's OpenSSH to the path, so we can use 1password as authentication helper
@@ -23,20 +37,27 @@ if [ -d "$windows_apps_path" ]; then
 	alias winget='winget.exe'
 fi
 
-# Find the 1Password CLI winget install path
-op_path="$(fd 'AgileBits.1Password.CLI' -p -t d "$windows_root/Users/$windows_user/AppData/Local/Microsoft/WinGet")"
-ssh_path="$windows_root/Windows/System32/OpenSSH"
+# win32yank
+if add_winget_package_path 'equalsraf.win32yank'; then
+	alias win32yank='win32yank.exe'
+fi
 
-if [ -d "$op_path" ] && [ -d "$ssh_path" ]; then
-	export PATH="$PATH:$op_path:$ssh_path"
+# Find the 1Password CLI winget install path
+if add_winget_package_path 'AgileBits.1Password.CLI'; then
 	alias op='op.exe'
 	eval "$(op.exe completion zsh)"
 	compdef _op op.exe
 
-	# This is used for 1password support
-	# See: https://developer.1password.com/docs/ssh/integrations/wsl/#optional-add-an-alias-for-ssh-commands
-	alias ssh='ssh.exe'
-	alias ssh-add='ssh-add.exe'
+	ssh_path="$windows_root/Windows/System32/OpenSSH"
+
+	if [ -d "$ssh_path" ]; then
+		export PATH="$PATH:$ssh_path"
+
+		# This is used for 1password support
+		# See: https://developer.1password.com/docs/ssh/integrations/wsl/#optional-add-an-alias-for-ssh-commands
+		alias ssh='ssh.exe'
+		alias ssh-add='ssh-add.exe'
+	fi
 fi
 
 # Docker
