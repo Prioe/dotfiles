58c4ad33989e392b2f2aba5d847020b3fd7457f3
Michael Albertz <michael@prio.one>
2024-03-14 10:41:34 +0100

    Add all astro language packs

--- astronvim_v4/lua/community.lua
+++ astronvim_v4/lua/community.lua
@@ -9,5 +9,20 @@ return {
   { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.motion.nvim-surround" },
-  -- import/override with your plugins folder
+
+  -- language packs: https://github.com/AstroNvim/astrocommunity/tree/main/lua/astrocommunity/pack
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.python-ruff" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.docker" },
+
+  { import = "astrocommunity.pack.typescript-all-in-one" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.markdown" },
+
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.toml" },
 }
