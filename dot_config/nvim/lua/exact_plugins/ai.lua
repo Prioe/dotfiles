5553c963e22efd84285b7d048a45e60692080406
Michael Albertz <michael@prio.one>
2025-10-10 09:38:13 +0200

    feat(nvim): replace useless ai tools with opencode

--- dot_config/nvim/lua/exact_plugins/ai.lua
+++ dot_config/nvim/lua/exact_plugins/ai.lua
@@ -1,22 +1,6 @@
 ---@type LazySpec
 return {
+  { import = "astrocommunity.ai.opencode-nvim" },
   -- { import = "astrocommunity.completion.avante-nvim" },
   -- { import = "astrocommunity.editing-support.copilotchat-nvim" },
-  { import = "astrocommunity.completion.copilot-lua-cmp" },
-  {
-    "zbirenbaum/copilot.lua",
-    ---@type copilot_config
-    opts = {
-      suggestion = {
-        debounce = 75,
-      },
-      filetypes = {
-        yaml = true,
-        markdown = true,
-        gitcommit = true,
-        gitrebase = true,
-        hgcommit = true,
-      },
-    },
-  },
 }
