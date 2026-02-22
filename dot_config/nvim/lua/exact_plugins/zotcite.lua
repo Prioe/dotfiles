3ea23e7528e7a745f709ede023c4305ee2b63b66
Michael Albertz <michael@prio.one>
2026-02-22 17:05:23 +0100

    chore(nvim): add patch for zotcite to work with zotero 8

--- dot_config/nvim/lua/exact_plugins/zotcite.lua
+++ dot_config/nvim/lua/exact_plugins/zotcite.lua
@@ -1,11 +1,35 @@
 ---@type LazySpec
 return {
   "jalvesaq/zotcite",
+  -- Zotero 8 moved citation keys from better-bibtex.sqlite into the main DB.
+  -- Remove this patch once https://github.com/jalvesaq/zotcite handles Zotero 8+ natively.
+  build = "git apply " .. vim.fn.stdpath("config") .. "/patches/zotcite-zotero8-citationkeys.patch",
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
     "nvim-telescope/telescope.nvim",
+    {
+      "AstroNvim/astrocore",
+      ---@type AstroCoreOpts
+      opts = {
+        mappings = {
+          n = {
+            ["<leader>z"] = { name = " Zotero" },
+            ["<leader>zo"] = { "<Plug>ZOpenAttachment", desc = "[O]pen attachment" },
+            ["<leader>zi"] = { "<Plug>ZCitationInfo", desc = "Citation [i]nfo" },
+            ["<leader>za"] = { "<Plug>ZCitationCompleteInfo", desc = "[A]ll reference fields" },
+            ["<leader>zb"] = { "<Plug>ZExtractAbstract", desc = "Insert a[b]stract" },
+            ["<leader>zv"] = { "<Plug>ZViewDocument", desc = "[V]iew document" },
+          },
+          i = {
+            ["<C-x><C-b>"] = { "<Plug>ZCite", desc = "Insert citation key" },
+          },
+        },
+      },
+    },
   },
   config = function()
-    require("zotcite").setup({})
+    require("zotcite").setup({
+      key_type = "better-bibtex",
+    })
   end,
 }
