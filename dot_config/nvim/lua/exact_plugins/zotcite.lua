91209e9bb23d092ad238eb05c1a4532d96031f72
Michael Albertz <michael@prio.one>
2026-02-23 09:31:11 +0100

    feat(nvim): skip zotcite if zotero not found

--- dot_config/nvim/lua/exact_plugins/zotcite.lua
+++ dot_config/nvim/lua/exact_plugins/zotcite.lua
@@ -1,6 +1,11 @@
 ---@type LazySpec
 return {
   "jalvesaq/zotcite",
+  cond = vim.uv.fs_access(
+    vim.env.APPDATA and vim.env.APPDATA .. "/Zotero/Zotero/profiles.ini"
+      or vim.fn.expand("~/.zotero/zotero/profiles.ini"),
+    "r"
+  ) or vim.uv.fs_access(vim.fn.expand("~/Library/Application Support/Zotero/profiles.ini"), "r") or false,
   -- Zotero 8 moved citation keys from better-bibtex.sqlite into the main DB.
   -- Remove this patch once https://github.com/jalvesaq/zotcite handles Zotero 8+ natively.
   build = "git apply " .. vim.fn.stdpath("config") .. "/patches/zotcite-zotero8-citationkeys.patch",
